9c49a6922f6fe5920974f08f8187c9217ac6ec81
Aishwarya-10-A <aishwarya.apsingikar@gmail.com>
2024-01-17 12:32:28 +0530

    Maven Java Push

---
+++ src/main/java/Package/Java/App.java
@@ -0,0 +1,13 @@
+package Package.Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
